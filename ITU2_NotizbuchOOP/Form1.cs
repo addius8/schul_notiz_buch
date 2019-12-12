002cc328f8282a04262441cf9a40a4d5f6643bfe
Adrian Müller <AMller@bkh>
2019-12-12 07:49:20 +0100

    hier master

---
+++ ITU2_NotizbuchOOP/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITU2_NotizbuchOOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateErstellung_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drpDwnKategorie_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<string> must_have_list_methode()
+        {
+            List<string> must_have_list = new List<string>();
+            must_have_list.Add("cu_title");
+            must_have_list.Add("category");
+            return must_have_list;
+        }
+
+
+
+        private void check_values()
+        {
+            List<string> must_have_list = must_have_list_methode();
+
+
+        }
+
+        // private void btnSpeichern_Click(object sender, EventArgs e)
+        private void btnSpeichern_notiz_Click(object sender, EventArgs e)
+        {
+
+            Console.WriteLine("Btn saved Event");
+            string cu_title = titel_notiz.Text;
+            string cu_priority = priority_notiz.Value.ToString();
+            string category = kategorie_notiz.Text;
+            string content = inhalt_notiz.Text;
+            DateTime create_date = erstellung_notiz.Value;
+            DateTime due_date = deadline_notiz.Value;
+            Console.WriteLine("cu_title: " + cu_title);
+            Console.WriteLine("cu_priority : " + cu_priority);
+            Console.WriteLine("category : " + category);
+            Console.WriteLine("create_date : " + create_date);
+            Console.WriteLine("due_date : " + due_date);
+            Console.WriteLine("content : " + content);
+
+            List<string> must_have_list = must_have_list_methode();
+
+
+            int size_of_must_have_list = must_have_list.Count;
+            for (int i = 0; i < size_of_must_have_list; i++)
+            {
+                Console.WriteLine(must_have_list[i]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numPrioSuche_notiz_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void btnSpeichern_notiz_Click(object sender, EventArgs e)
+        //{
+        //
+        //}
+
+        private void inhalt_notiz_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ITU2_NotizbuchOOP/Notiz.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITU2_NotizbuchOOP
+{
+    class Notiz
+    {
+        private string titel;
+        private int prio;
+        private string kategorie;
+        private string text;
+        private DateTime erstellDatum;
+        private DateTime deadLine;
+
+        public void setTitle(string p_titel)
+        {
+            titel = p_titel;
+        }
+
+        public void setPrio(int p_prio)
+        {
+            prio = p_prio;
+        }
+
+        public void setKategorie(string p_kategorie)
+        {
+            kategorie = p_kategorie;
+        }
+
+        public void setText(string p_text)
+        {
+            text = p_text;
+        }
+
+        public void setErstellDatum(DateTime p_erstellDatum)
+        {
+            erstellDatum = p_erstellDatum;
+
+        }
+
+        public void setDeadLine(DateTime p_deadLine)
+        {
+            deadLine = p_deadLine;
+        }
+
+        /**
+        public Notiz(
+                string titel,
+                int prio,
+                string kategorie,
+                string text,
+                DateTime erstellDatum,
+                DateTime deadLine
+            )
+        {
+            this.titel = titel;
+            this.prio = prio;
+            this.kategorie = kategorie;
+            this.text = text;
+            this.erstellDatum = erstellDatum;
+            this.deadLine = deadLine;
+            
+        }
+         *
+         */
+    }
+}
